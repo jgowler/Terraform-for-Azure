d91065de85e788131ac67a53f3fd7a801b7b04f1
jgowler <64489787+jgowler@users.noreply.github.com>
2025-10-18 14:50:14 +0100

    Created template files
    secrets kept elsewhere, variables used to reference values

---
+++ advanced/variables.tf
@@ -0,0 +1,20 @@
+variable "subscription_id" {
+    type = string
+    default = ""
+    sensitive = true
+}
+variable "tenant_id" {
+    type = string
+    default = ""
+    sensitive = true
+}
+variable "service_account_id" {
+    type = string
+    default = ""
+    sensitive = true
+}
+variable "service_account_secret" {
+    type = string
+    default = ""
+    sensitive = true
+}
